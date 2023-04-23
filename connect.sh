009bd4eedb7b08b4582a9ab971409962f0d524e1
alex <format37@gmail.com>
2023-04-24 00:34:32 +0400

    deleted:    connect.sh

--- connect.sh
+++
@@ -1,4 +0,0 @@
-# get id of container with the corresponding name and store to variable id
-id=$(sudo docker ps -a | grep stablelm | awk '{print $1}')
-# connect bash to container with id $id
-sudo docker exec -it $id /bin/bash
