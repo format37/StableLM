4fa45ef9618b711e78b7e379be11e952b559cdaa
alex <format37@gmail.com>
2023-04-24 01:20:21 +0400

    modified:   app.py

--- app.py
+++ app.py
@@ -6,14 +6,12 @@ import logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-# Choose model name
-model_name = "stabilityai/stablelm-tuned-alpha-7b" #@param ["stabilityai/stablelm-tuned-alpha-7b", "stabilityai/stablelm-base-alpha-7b", "stabilityai/stablelm-tuned-alpha-3b", "stabilityai/stablelm-base-alpha-3b"]
+model_name = "stabilityai/stablelm-tuned-alpha-7b"
 
 logger.info(f"Using `{model_name}`")
 
-# Select "big model inference" parameters
-torch_dtype = "float16" #@param ["float16", "bfloat16", "float"]
-load_in_8bit = False #@param {type:"boolean"}
+torch_dtype = "float16"
+load_in_8bit = False
 device_map = "auto"
 
 logger.info(f"Loading with: `{torch_dtype=}, {load_in_8bit=}, {device_map=}`")
@@ -35,15 +33,8 @@ class StopOnTokens(StoppingCriteria):
                 return True
         return False
 
-# Sampling args
-max_new_tokens = 128 #@param {type:"slider", min:32.0, max:3072.0, step:32}
-temperature = 0.7 #@param {type:"slider", min:0.0, max:1.25, step:0.05}
-top_k = 0 #@param {type:"slider", min:0.0, max:1.0, step:0.05}
-top_p = 0.9 #@param {type:"slider", min:0.0, max:1.0, step:0.05}
-do_sample = True #@param {type:"boolean"}
-
-def generate_text(system_prompt, user_prompt):
-    prompt = f"{system_prompt}{user_prompt}"
+def generate_text(system_prompt, user_prompt, top_k, top_p, temperature, max_new_tokens, do_sample):
+    prompt = f"{system_prompt}<|USER|>{user_prompt}<|ASSISTANT|>"
     
     inputs = tokenizer(prompt, return_tensors="pt")
     inputs.to(model.device)
@@ -65,20 +56,41 @@ def generate_text(system_prompt, user_prompt):
     return completion
 
 def main():
-    system_prompt_input = gr.inputs.Textbox(lines=4, label="Enter a system prompt")
-    user_prompt_input = gr.inputs.Textbox(lines=2, label="Enter a user prompt")
+    system_prompt = """<|SYSTEM|># StableLM Tuned (Alpha version)
+        - StableLM is a helpful and harmless open-source AI language model developed by StabilityAI.
+        - StableLM is excited to be able to help the user, but will refuse to do anything that could be considered harmful to the user.
+        - StableLM is more than just an information source, StableLM is also able to write poetry, short stories, and make jokes.
+        - StableLM will refuse to participate in anything that could harm a human.
+        """
+    system_prompt_input = gr.inputs.Textbox(lines=4, label="Enter a system prompt", default=system_prompt)
+
+    user_prompt = "Can you write a song about a pirate at sea?"
+    user_prompt_input = gr.inputs.Textbox(lines=2, label="Enter a user prompt", default=user_prompt)
+
+    top_k_slider = gr.inputs.Slider(minimum=0.0, maximum=100.0, step=1, default=0.0, label="Top-k")
+    top_p_slider = gr.inputs.Slider(minimum=0.0, maximum=1.0, step=0.05, default=0.9, label="Top-p")
+    temperature_slider = gr.inputs.Slider(minimum=0.0, maximum=1.25, step=0.05, default=0.7, label="Temperature")    
+    max_new_tokens_slider = gr.inputs.Slider(minimum=32.0, maximum=3072.0, step=32.0, default=64.0, label="Max new tokens")
+    do_sample_slider = gr.inputs.Checkbox(label="Do sample", default=True)
     output_textbox = gr.outputs.Textbox(label="Generated text")
 
     iface = gr.Interface(
         fn=generate_text,
-        inputs=[system_prompt_input, user_prompt_input],
+        inputs=[
+            system_prompt_input, 
+            user_prompt_input,
+            top_k_slider, 
+            top_p_slider,
+            temperature_slider,
+            max_new_tokens_slider,
+            do_sample_slider
+            ],
         outputs=output_textbox,
         title="Text Generation with StableLM",
         description="Generate text with the StableLM language model. Enter a prompt and get a response.",
     )
 
     iface.launch()
-
 
 if __name__ == "__main__":
     main()
